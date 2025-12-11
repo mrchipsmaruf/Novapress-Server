3ed348b8d396f585210a80a80713845132efa36d
mrchipsmaruf <mrchipsmaruf@gmail.com>
2025-12-11 23:40:35 +0600

    added user apis

--- index.js
+++ index.js
@@ -2,11 +2,98 @@ const express = require('express')
 let cors = require('cors');
 const app = express();
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const port = process.env.PORT || 3000
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rjffgqf.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        let db = client.db('novapress_db');
+        let usersCollection = db.collection("users");
+        let issuesCollection = db.collection("issues");
+        let assignmentsCollection = db.collection("assignments");
+        let feedbackCollection = db.collection("feedback");
+        let timelineCollection = db.collection("timeline");
+        let paymentsCollection = db.collection("payments");
+
+
+
+        //USERS APIS
+        //CREATE USER (Signup)
+        app.post('/users', async (req, res) => {
+            try {
+                let user = req.body;
+                const existingUser = await usersCollection.findOne({ email: user.email });
+
+                if (existingUser) {
+                    return res.send({ message: "User already exists", insertedId: null });
+                }
+
+                const result = await usersCollection.insertOne(user);
+                res.send(result);
+            }
+            catch (error) {
+                res.status(500).send({ error: error.message });
+            }
+        })
+
+        // GET USER BY EMAIL
+        app.get('/users/:email', async (req, res) => {
+            try {
+                const email = req.params.email;
+                const user = await usersCollection.findOne({ email });
+                res.send(user);
+            }
+            catch (error) {
+                res.status(500).send({ error: error.message });
+            }
+        })
+
+        // UPDATE USER ROLE
+        app.patch('/users/:email', async (req, res) => {
+            try {
+                const email = req.params.email;
+                const role = req.body.role;
+
+                const result = await usersCollection.updateOne(
+                    { email },
+                    { $set: { role } }
+                );
+
+                res.send(result);
+            } catch (error) {
+                res.status(500).send({ error: error.message });
+            }
+        });
+
+        
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
